9fae6a04c3d399b1c6a23dd8f6c8b92b59c58b1b
Yunhang Shen (MSR Student-Person Consulting) <v-yunshe@microsoft.com>
2017-03-04 15:13:16 +0800

    update install.sh to count the number of poems

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 set -e
-set -x
+#set -x
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 DATA_DIR="${DIR}/data"
@@ -9,10 +9,20 @@ FORTUNES="/usr/share/games/fortunes/"
 
 if [ -d ${DATA_DIR} ]
 then
-	rm -r ${DATA_DIR}
+	rm -r ${DATA_DIR}/*
 fi
 
 time python ${DIR}/constructer.py
+
+
+printf "\x1B[33m"
+for f in ${DATA_DIR}/*
+do
+	printf "%2s${f}:\t" 
+	grep -c '^%' ${f}
+done
+
+printf "\x1B[m"
 
 for f in ${DATA_DIR}/*
 do
@@ -20,3 +30,5 @@ do
 done
 
 sudo cp ${DATA_DIR}/* ${FORTUNES}
+
+
